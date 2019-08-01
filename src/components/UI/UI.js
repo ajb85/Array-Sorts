16ab2f60d59fad41388ca5bbdd434d9c82a2452e
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-01 19:04:49 -0400

    Adds more button controls

--- src/components/UI/UI.js
+++ src/components/UI/UI.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./styles.module.scss";
 
+import bubbleSort from "sorts/bubbleSort.js";
+
 function UI({ state, dispatch }) {
+  useEffect(() => {
+    dispatch({ type: "setSteps", payload: bubbleSort(state.array) });
+  }, [state.array, dispatch]);
+
   const updateLength = ({ target: { value } }) => {
     const num = parseInt(value, 10);
 
@@ -14,18 +20,42 @@ function UI({ state, dispatch }) {
       });
     }
   };
+
+  const nextStep = direction => {
+    if (direction < 0 && state.step > 0) {
+      dispatch({ type: "decrementStep" });
+    } else if (direction > 0 && state.step < state.steps.length - 1) {
+      dispatch({ type: "incrementStep" });
+    }
+  };
+
   return (
     <section className={styles.UI}>
-      <div>
-        <label>Array Length:</label>
-        <input value={state.length} onChange={e => updateLength(e)} />
+      <div className={styles.settings}>
+        <div>
+          <label>Array Length:</label>
+          <input value={state.length} onChange={e => updateLength(e)} />
+        </div>
+        <div>
+          <label>Randomize Array:</label>
+          <FontAwesomeIcon
+            onClick={() => dispatch({ type: "randomize" })}
+            icon="sync"
+          />
+        </div>
       </div>
-      <div>
-        <label>Randomize Array:</label>
+
+      <div className={styles.controls}>
         <FontAwesomeIcon
-          onClick={() => dispatch({ type: "randomize" })}
-          icon="sync"
+          // onClick={()=>dispatch({type:"play"})}
+          icon="play"
         />
+        <FontAwesomeIcon
+          // onClick={()=>dispatch({type:"pause"})}
+          icon="pause"
+        />
+        <FontAwesomeIcon onClick={() => nextStep(-1)} icon="step-backward" />
+        <FontAwesomeIcon onClick={() => nextStep(1)} icon="step-forward" />
       </div>
     </section>
   );
