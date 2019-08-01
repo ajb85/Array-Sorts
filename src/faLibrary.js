603de8d520e3b972e1fd8e5408c28d6b7dc85cd4
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-01 16:58:08 -0400

    Creates a Font Awesome library to use throughout the app

---
+++ src/faLibrary.js
@@ -0,0 +1,6 @@
+// Font Awesome Library -- allows for use throughout the app
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
+library.add(faSync);
+
+export default library;
