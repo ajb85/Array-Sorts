0e9594c58849551cdae1ed6d0a256c3c4d180d4d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-01 19:05:23 -0400

    Adds additional icons

--- src/faLibrary.js
+++ src/faLibrary.js
@@ -1,6 +1,12 @@
 // Font Awesome Library -- allows for use throughout the app
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSync } from "@fortawesome/free-solid-svg-icons";
-library.add(faSync);
+import {
+  faSync,
+  faPause,
+  faPlay,
+  faStepForward,
+  faStepBackward
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faSync, faPause, faPlay, faStepForward, faStepBackward);
 
 export default library;
