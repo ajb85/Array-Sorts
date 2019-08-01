8c04e163a4a119dbce67b4f7e7860b7645fbcb87
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-01 19:04:28 -0400

    Updates array to show conditions being evaluated

--- src/components/Array/Array.js
+++ src/components/Array/Array.js
@@ -3,16 +3,42 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 function Array({ state }) {
+  const thisStep = state.steps[state.step] ? state.steps[state.step] : null;
+  console.log(thisStep);
   return (
     <section className={styles.Array}>
-      <p>[</p>
-      {state.array.map((numInfo, i) => (
+      <div className={styles.condition}>
         <p>
-          {numInfo.num}
-          {i < state.array.length - 1 ? "," : ""}
+          {!thisStep || state.step === 0
+            ? "Press Play or Step Forward to begin"
+            : thisStep.evaluation}
         </p>
-      ))}
-      <p>]</p>
+      </div>
+      <div className={styles.displayArray}>
+        <p style={{ marginRight: "5px" }}>[</p>
+        {state.array.map((numInfo, i) => {
+          const background =
+            state.step > 0 && thisStep.indices.start.includes(i)
+              ? "tan"
+              : "white";
+          return (
+            <>
+              <p
+                key={numInfo.start}
+                style={{
+                  backgroundColor: background
+                }}
+              >
+                {numInfo.num}
+              </p>
+              <p style={{ marginRight: "5px" }}>
+                {i < state.array.length - 1 ? "," : ""}
+              </p>
+            </>
+          );
+        })}
+        <p style={{ marginRight: "5px" }}>]</p>
+      </div>
     </section>
   );
 }
